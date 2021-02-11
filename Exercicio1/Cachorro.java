70303e0e7d918e0841552800f3dc49214c43e6e3
valterleao <valterleao@hotmail.com>
2021-02-11 02:04:34 +0000

    Arquivos com correção no codigo java

--- Exercicio1/Cachorro.java
+++ Exercicio1/Cachorro.java
@@ -7,7 +7,7 @@ class Cachorro {
    int peso;
 
    void latir(){
-     System.out.println("O cachorro" + nome + " está latindo...");
+     System.out.println("O cachorro " + nome + " está latindo...");
    }
 
    void andar(int distancia){
--- Exercicio2/Main.java
+++ Exercicio2/Main.java
@@ -1,10 +1,10 @@
 class Main{
 
   public static void main(String[] args){
-    Carro.c1 = new Carro();
+    Carro c1 = new Carro();
     c1.cor = "Amarelo";
     c1.marca = "Ford";
     c1.movimentar();     
   }
-  
+
 }
