2339512cbdd84e60cbb37b171b8b4b531d6500c7
valterleao <valterleao@hotmail.com>
2021-02-18 20:23:33 +0000

    Atualizando Exercicio3_java

--- Exercicio1/Main.java
+++ Exercicio1/Main.java
@@ -1,11 +1,12 @@
 class Main {
 
   public static void main(String[] args){
-    Cachorro.c1 = new Cachorro();
+    Cachorro c1 = new Cachorro();
     c1.nome = "Safira";
     c1.idade = 10;
+    c1.latir();
     
-    Cachorro.c2 = new Cachorro();
+    Cachorro c2 = new Cachorro();
     c2.nome = "Bolt";
     c2.idade = 8;
    
