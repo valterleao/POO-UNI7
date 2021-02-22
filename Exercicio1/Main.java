b7e60dd12d1cf14d5d8bfbf098e5f104f3c48759
repl.it user <replituser@example.com>
2021-02-22 21:54:34 +0000

    Atualizando exercicios

--- Exercicio1/Main.java
+++ Exercicio1/Main.java
@@ -1,14 +1,21 @@
 class Main {
 
   public static void main(String[] args){
-    Cachorro c1 = new Cachorro();
-    c1.nome = "Safira";
-    c1.idade = 10;
+
+    Cachorro c1 = new Cachorro(Raca.MALTES,"Rex");
+    c1.setIdade(10);
     c1.latir();
+    c1.andar(1);
+
+    Cachorro c2 = new Cachorro(Raca.DOG_ALEMAO,"Babau", 10, 10, 3);
+    c2.latir();
     
-    Cachorro c2 = new Cachorro();
-    c2.nome = "Bolt";
-    c2.idade = 8;
-   
+    for (int i = 0; i<10; i++){
+      c1.latir();
+      if (i%2 == 0){
+        c2.latir();
+      }
+    }
+
   }
 }
